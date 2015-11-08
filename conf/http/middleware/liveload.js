b7160d0d5884c96f51ad20911a69e160ac04467a
dodio <go2solo18@gmail.com>
2015-11-09 02:17:47 +0800

    不同的项目不同的端口

--- conf/http/middleware/liveload.js
+++ conf/http/middleware/liveload.js
@@ -3,5 +3,8 @@ module.exports.liveload = {
 		return [server.ROOT_PATH,"/views"].join("/");
 	},
 	files : /.(js|css|html|less|tpl)$/,
-	excludes: /^node_modules$/
+	excludes: /^node_modules$/,
+  port : function(server) {
+    return parseInt( Math.random() * 1e4 + 2e4 ) ;
+  }
 }
--- middleware/liveload.js
+++ middleware/liveload.js
@@ -2,5 +2,6 @@ var liveload = require("liveload");
 var _ = require("lodash");
 module.exports = function(conf,server) {
 	conf.root = server.module("util").dynamicConfig(conf.root,server);
+  conf.port = conf.port(server);
 	return liveload(conf);
 }
