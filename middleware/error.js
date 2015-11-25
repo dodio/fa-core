ff982e7161bddeaffb00faf728476561be13613c
xuyuanzhi <xuyuanzhi@danlu.com>
2015-11-25 19:06:51 +0800

    使用rootFa.IS_DEV

--- middleware/error.js
+++ middleware/error.js
@@ -3,7 +3,7 @@ var express = require("express");
 module.exports = function (conf,server) {
     var app = express.Router();
     // 不是开发模式，记录错误日志
-    if ( server.ENV != "dev") {
+    if ( !server.IS_DEV ) {
         var logger = server.module("log");
         app.use(function(error,req,res,next){
             logger.fatal(error);
