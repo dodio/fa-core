94f9036edb5c4b37461581edcb9d6ef2563fcd36
dodio <go2solo18@gmail.com>
2015-11-28 19:04:04 +0800

    修复不能正常使用500错误的bug

--- middleware/error.js
+++ middleware/error.js
@@ -1,23 +1,22 @@
 var debuglog = require('debuglog')('fa/log');
 var express = require("express");
 module.exports = function (conf,server) {
-    var app = express.Router();
+    var app = [];
     // 不是开发模式，记录错误日志
     if ( !server.IS_DEV ) {
         var logger = server.module("log");
-        app.use(function(error,req,res,next){
+        app.push(function(error,req,res,next){
             logger.fatal(error);
             next(error);
         });
-        app.use(conf.handler);
     } else {
         debuglog('start debug mode for error page'.yellow);
-        app.use(function(error,req,res,next){
+        app.push(function(error,req,res,next){
             console.log(error);
             next(error);
         });
-        app.use(conf.handler);
     }
+    app.push(conf.handler);
     return app;
 };
 
