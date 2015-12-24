00e9c407375afb2c325fa67a89a9978813dc9da9
xuyuanzhi <xuyuanzhi@danlu.com>
2015-12-24 15:17:00 +0800

    修复bug，日志模块设置方式不正确.

--- plugins/log.js
+++ plugins/log.js
@@ -8,9 +8,7 @@ module.exports = function(server, conf){
     var conf = _.mapValues(conf,function(v){
         return util.dynamicConfig(v,server);
     });
-    server.module.setStatic("log",function(){
-        return logger.getLogger(conf);
-    });
+    server.module.setStatic("log",logger.getLogger(conf));
     server.middleware.setStatic("log",function(){
         return logger(conf);
     });
